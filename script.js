a5f8a3f5282d6e40d787f88df66917398a5a820f
CsongetoCsongor <csongeto.csongor@students.jedlik.eu>
2024-04-23 14:03:27 +0200

    NUMBER

--- script.js
+++ script.js
@@ -37,6 +37,55 @@ const cvsHeight = 600;
 
 
 
+
+
+//CREATE
+
+if(!localStorage.getItem('prevGames')) {
+
+    let previousGames = [];
+    let previousGamesString = JSON.stringify(previousGames);
+
+    localStorage.setItem('prevGames', previousGamesString);
+  
+  }
+
+// let previousGames = [];
+// let previousGamesString = JSON.stringify(previousGames);
+
+// localStorage.setItem('prevGames', previousGamesString);
+
+
+
+// localStorage.clear();
+
+let existingArray;
+let newArrayString;
+let getPreviousGamesString;
+
+// //EXPAND
+// let existingArray = JSON.parse(localStorage.getItem('prevGames'));
+// existingArray.push([7,8,9]);
+
+// let newArrayString = JSON.stringify(existingArray);
+// localStorage.setItem('prevGames', newArrayString);
+
+// //GET
+// let getPreviousGamesString = localStorage.getItem('prevGames');
+// getPreviousGamesString = getPreviousGamesString.slice(1, -1);
+// // let getPreviousGames = JSON.parse(getPreviousGamesString);
+
+// console.log(getPreviousGamesString.slice(1, -1));
+// console.log(stringToArray(getPreviousGamesString)[0], stringToArray(getPreviousGamesString)[1], stringToArray(getPreviousGamesString)[2]);
+
+
+
+
+
+
+let currentTime;
+
+
 const img_bad_luck = new Image();
 img_bad_luck.src = 'images/stock_crash_bad_luck.png'; 
 
@@ -55,8 +104,36 @@ let bet;
 let multiplier = 1;
 let inflation = new Inflation();
 
+function stringToArray(str) {
+    // Remove the surrounding square brackets
+    str = str.slice(1, -1);
+    
+    // Split the string on commas
+    return str.split(',');
+}
+  
+function insertLetter(string, letter, position) {
+    return string.slice(0, position) + letter + string.slice(position);
+  }
+
 function inflate() {
 
+
+    const timeString = new Date().toLocaleString('en-US', {timeZone: 'Europe/Helsinki'}).slice(11,19).replace(/:/g, ':');
+
+    
+
+    let hours = timeString.substring(0, 2);
+    hours = parseInt(hours) - 1;
+    if(hours < 10) {
+    hours = '0' + hours+":";
+    }
+
+    const updatedTime = hours + timeString.substring(2); 
+
+
+
+    currentTime = updatedTime;
     if (bet.autoCashoutValue <= multiplier) {
         clearInterval(interval);
         inflation = new Inflation();
@@ -84,6 +161,36 @@ function inflate() {
 
         cvs.style.boxShadow = '0 0 50px 15px green'
 
+        //EXPAND
+        // existingArray = stringToArray(localStorage.getItem('prevGames'));
+        // existingArray.push(["WIN","+"+Math.round(parseFloat(bet.value * bet.autoCashoutValue)),new Date().toLocaleString('en-US', {timeZone: 'Europe/Helsinki'}).slice(11,19).replace(/:/g, ':')    ]);
+
+        // newArrayString = JSON.stringify(existingArray);
+        // localStorage.setItem('prevGames', newArrayString);
+
+        // //GET
+        // getPreviousGamesString = localStorage.getItem('prevGames');
+        // getPreviousGamesString = getPreviousGamesString.slice(1, -1);
+
+        let existingArray = JSON.parse(localStorage.getItem('prevGames'));
+        existingArray.push(["WIN","+"+Math.round(parseFloat(bet.value * bet.autoCashoutValue)),currentTime]);
+
+        let newArrayString = JSON.stringify(existingArray);
+        localStorage.setItem('prevGames', newArrayString);
+
+        let arrayString = localStorage.getItem('prevGames');
+        let arrayOfArrays = JSON.parse(arrayString); 
+
+        
+
+
+
+
+        
+        arrayOfArrays.forEach(element => {
+            console.log(element[0], element[1], element[2]);
+        });
+
         updateStats();
         
         // balance
@@ -106,6 +213,29 @@ function inflate() {
         // balance -= bet.value;
         // localStorage.setItem("balance", balance);
         document.cookie = Number(document.cookie) - Number(bet.value);
+
+
+        let existingArray = JSON.parse(localStorage.getItem('prevGames'));
+        existingArray.push(["LOSE","-"+bet.value,currentTime]);
+
+        let newArrayString = JSON.stringify(existingArray);
+        localStorage.setItem('prevGames', newArrayString);
+
+        let arrayString = localStorage.getItem('prevGames');
+        let arrayOfArrays = JSON.parse(arrayString); 
+
+        
+
+
+
+
+        
+        arrayOfArrays.forEach(element => {
+            console.log(element[0], element[1], element[2]);
+        });
+
+
+
         updateStats();
         updateStats();
         // console.log(localStorage.getItem("balance"));
@@ -144,9 +274,10 @@ betButton.addEventListener('click', startInflation);
 
 function startInflation() {
     
+
     // console.log("betValue.value: " + betValue.value);
     // console.log("localStorage.getItem(balance): " + localStorage.getItem("balance"));
-    if (Number(betValue.value) <= Number(document.cookie) && Number(betValue.value) > 0 && autoCashoutValue.value >= 2) {
+    if (Number(betValue.value) <= Number(document.cookie) && Number(betValue.value) > 0 && (autoCashoutValue.value >= 2 || autoCashoutValue.value == "")) {
         if (betValue.value != "") {
             if (isButtonCashout == false) {
                 bet = new Bet(betValue.value, autoCashoutValue.value);
@@ -169,6 +300,21 @@ function startInflation() {
                 // updateStats();
                 // console.log(localStorage.getItem("balance"));
                 document.cookie = Number(document.cookie) + Number(bet.value * multiplier);
+
+
+                let existingArray = JSON.parse(localStorage.getItem('prevGames'));
+                existingArray.push(["WIN","+"+bet.value * multiplier,currentTime]);
+
+                let newArrayString = JSON.stringify(existingArray);
+                localStorage.setItem('prevGames', newArrayString);
+
+                let arrayString = localStorage.getItem('prevGames');
+                let arrayOfArrays = JSON.parse(arrayString); 
+
+                arrayOfArrays.forEach(element => {
+                    console.log(element[0], element[1], element[2]);
+                });
+
                 updateStats();
                 
                 outcomeText.innerHTML = "CASHED OUT MANUALLY"
@@ -204,7 +350,7 @@ function startInflation() {
 }
 
 function updateStats() {
-    balanceText.innerHTML = Number(document.cookie) + "Ft";
+    balanceText.innerHTML = Number(document.cookie).toFixed() + "Ft";
     // localStorage.setItem("balance", balance);
     // console.log(localStorage.getItem("balance"));
 }
